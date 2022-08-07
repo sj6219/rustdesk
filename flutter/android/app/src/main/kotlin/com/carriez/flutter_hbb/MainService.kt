970e6dccaf76913adde0009d9c936e42857652eb
sjpark <sj6219@hotmail.com>
2022-08-07 18:15:02 +0900

    enable sleep

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainService.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainService.kt
@@ -381,17 +381,17 @@ class MainService : Service() {
         setFrameRawEnable("video",true)
         setFrameRawEnable("audio",true)
         
-        timerTask = kotlin.concurrent.timer(initialDelay = 2000, period = 2000) {	
-            if (!powerManager.isInteractive) {
-                Log.d(logTag,"Turn on Screen!!!")
-            }
-            if (wakeLock.isHeld) {
-                //Log.d(logTag,"Turn on Screen, WakeLock release")
-                wakeLock.release()
-            }
-            //Log.d(logTag,"Turn on Screen")
-            wakeLock.acquire(5000)   
-        }
+        // timerTask = kotlin.concurrent.timer(initialDelay = 2000, period = 2000) {	
+        //     if (!powerManager.isInteractive) {
+        //         Log.d(logTag,"Turn on Screen!!!")
+        //     }
+        //     if (wakeLock.isHeld) {
+        //         //Log.d(logTag,"Turn on Screen, WakeLock release")
+        //         wakeLock.release()
+        //     }
+        //     //Log.d(logTag,"Turn on Screen")
+        //     wakeLock.acquire(5000)   
+        // }
         return true
     }
 
