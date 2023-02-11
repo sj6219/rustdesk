73566b8d9eb058af12896ebf4ff26a10f0a3fb5b
sjpark <sj6219@hotmail.com>
2023-02-12 06:07:26 +0900

    bug fix for swap key

--- src/ui_session_interface.rs
+++ src/ui_session_interface.rs
@@ -370,15 +370,15 @@ impl<T: InvokeUiSession> Session<T> {
 
                 let key = match peer.as_str() {
                     "windows" => {
-                        let key = rdev::win_key_from_code(code);
+                        let key = rdev::win_key_from_scancode(code);
                         let key = match key {
-                            rdev::Key::ControlLeft => rdev::Key::MetaLeft,
+                            rdev::Key::ControlLeft => rdev::Key::MetaLeft, 
                             rdev::Key::MetaLeft => rdev::Key::ControlLeft,
                             rdev::Key::ControlRight => rdev::Key::MetaLeft,
                             rdev::Key::MetaRight => rdev::Key::ControlLeft,
                             _ => key,
                         };
-                        rdev::win_keycode_from_key(key).unwrap_or_default()
+                        rdev::win_scancode_from_key(key).unwrap_or_default()
                     }
                     "macos" => {
                         let key = rdev::macos_key_from_code(code);
